d8646b58a09cf0ebb1fc1435c7d97bf97a7ef61d
Kazark <kazark@zoho.com>
2014-07-16 18:46:07 -0400

    Move up and down

--- Move.hs
+++ Move.hs
@@ -2,3 +2,5 @@ module Move where
 
 moveLeft coordinates = (fst coordinates - 1, snd coordinates)
 moveRight coordinates = (fst coordinates + 1, snd coordinates)
+moveUp coordinates = (fst coordinates, snd coordinates - 1)
+moveDown coordinates = (fst coordinates, snd coordinates + 1)
--- MoveSpec.hs
+++ MoveSpec.hs
@@ -13,3 +13,11 @@ main = hspec $ do
         it "should return increment the x-coordinate" $do
             moveRight (1, 3) `shouldBe` (2, 3)
             moveRight (2, 2) `shouldBe` (3, 2)
+    describe "Move up" $do
+        it "should return decrement the y-coordinate" $do
+            moveUp (1, 3) `shouldBe` (1, 2)
+            moveUp (2, 2) `shouldBe` (2, 1)
+    describe "Move down" $do
+        it "should return increment the y-coordinate" $do
+            moveDown (1, 3) `shouldBe` (1, 4)
+            moveDown (2, 2) `shouldBe` (2, 3)
