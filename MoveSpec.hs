8c171a113eb1ef817747a3cf255b4c318c6d46ce
Kazark <kazark@zoho.com>
2014-07-16 21:54:59 -0400

    Named movement specs better upon review

--- MoveSpec.hs
+++ MoveSpec.hs
@@ -6,18 +6,18 @@ import Move
 main :: IO ()
 main = hspec $ do
     describe "Move left" $do
-        it "should return decrement the x-coordinate" $do
+        it "should return the coordinates with the column decremented" $do
             moveLeft (ColumnAndRow 1 3) `shouldBe` (ColumnAndRow 0 3)
             moveLeft (ColumnAndRow 2 2) `shouldBe` (ColumnAndRow 1 2)
     describe "Move right" $do
-        it "should return increment the x-coordinate" $do
+        it "should return the coordinates with the column incremented" $do
             moveRight (ColumnAndRow 1 3) `shouldBe` (ColumnAndRow 2 3)
             moveRight (ColumnAndRow 2 2) `shouldBe` (ColumnAndRow 3 2)
     describe "Move up" $do
-        it "should return decrement the y-coordinate" $do
+        it "should return the coordinates with the row decremented" $do
             moveUp (ColumnAndRow 1 3) `shouldBe` (ColumnAndRow 1 2)
             moveUp (ColumnAndRow 2 2) `shouldBe` (ColumnAndRow 2 1)
     describe "Move down" $do
-        it "should return increment the y-coordinate" $do
+        it "should return the coordinates with the row incremented" $do
             moveDown (ColumnAndRow 1 3) `shouldBe` (ColumnAndRow 1 4)
             moveDown (ColumnAndRow 2 2) `shouldBe` (ColumnAndRow 2 3)
