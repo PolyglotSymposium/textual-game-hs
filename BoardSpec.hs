b657aa7b0ca74237a4c51fbd8d4a07942a2fa652
Kazark <kazark@zoho.com>
2014-09-11 22:20:40 -0400

    Reworded cell display specs

--- BoardSpec.hs
+++ BoardSpec.hs
@@ -10,12 +10,12 @@ main = hspec $ do
         context "which is plain and empty" $do
             it "appears empty" $do
                 displayCell plainEmptyCell `shouldBe` " "
-        context "which has the user in it" $do
-            it "appears as the at sign" $do
+        context "which has a creature in it" $do
+            it "appears as the creature's symbol" $do
                 displayCell (NewCell User NoItem) `shouldBe` "@"
-        context "with money it it" $do
-            it "appears as the dollar sign" $do
+        context "which has an item in it" $do
+            it "appears as the item's symbol" $do
                 displayCell (NewCell NoCreature Money) `shouldBe` "$"
-        context "with the user and money in it" $do
-            it "appears as the dollar sign" $do
+        context "which has a creature and an item in it" $do
+            it "appears as the creature's symbol" $do
                 displayCell (NewCell User Money) `shouldBe` "@"
