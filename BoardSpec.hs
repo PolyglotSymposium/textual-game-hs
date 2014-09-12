313e427f184b8532824b0d64134f9aed274690bc
Kazark <kazark@zoho.com>
2014-09-11 22:18:15 -0400

    Clarified display if creature & item both in cell
    The creature displays instead of the item if they are both in the cell

--- BoardSpec.hs
+++ BoardSpec.hs
@@ -6,13 +6,16 @@ import Inventory
 
 main :: IO ()
 main = hspec $ do
-    describe "Displaying a cell" $do
+    describe "The display of a cell" $do
         context "which is plain and empty" $do
-            it "then it appears empty" $do
+            it "appears empty" $do
                 displayCell plainEmptyCell `shouldBe` " "
         context "which has the user in it" $do
-            it "then it appears as the at sign" $do
+            it "appears as the at sign" $do
                 displayCell (NewCell User NoItem) `shouldBe` "@"
         context "with money it it" $do
-            it "then it appears as the dollar sign" $do
+            it "appears as the dollar sign" $do
                 displayCell (NewCell NoCreature Money) `shouldBe` "$"
+        context "with the user and money in it" $do
+            it "appears as the dollar sign" $do
+                displayCell (NewCell User Money) `shouldBe` "@"
